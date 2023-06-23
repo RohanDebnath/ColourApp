cd4846b13ca2543e5eac5816578d55d244bf505c
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-06-23 17:09:04 +0530

    number showing dataset

--- app/src/main/java/com/example/colorapp/MainActivity.java
+++ app/src/main/java/com/example/colorapp/MainActivity.java
@@ -3,62 +3,47 @@ package com.example.colorapp;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import java.util.List;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Toast;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private MainViewModel viewModel;
     private DataListAdapter adapter;
+    private TextView tvPendingCount;
+    private DatabaseReference databaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         FloatingActionButton fab = findViewById(R.id.fab);
+
 
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         adapter = new DataListAdapter();
@@ -72,57 +57,97 @@ public class MainActivity extends AppCompatActivity {
             public void onChanged(List<DataModel> data) {
                 adapter.setData(data);
                 adapter.notifyDataSetChanged();
+
+                updatePendingCount(data.size());
             }
         });
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Add Data");
-
-                LinearLayout layout = new LinearLayout(MainActivity.this);
-                layout.setOrientation(LinearLayout.VERTICAL);
-                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.MATCH_PARENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT
-                );
-                params.setMargins(40, 0, 40, 0);
-
-                EditText colorEditText = new EditText(MainActivity.this);
-                colorEditText.setHint("Color Hash Code");
-                layout.addView(colorEditText, params);
-
-                EditText dateEditText = new EditText(MainActivity.this);
-                dateEditText.setHint("Date");
-                layout.addView(dateEditText, params);
-
-                builder.setView(layout);
-
-                builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        String colorHashCode = colorEditText.getText().toString();
-                        String date = dateEditText.getText().toString();
-
-                        if (!TextUtils.isEmpty(colorHashCode) && !TextUtils.isEmpty(date)) {
-                            viewModel.insertData(colorHashCode, date);
-                        } else {
-                            Toast.makeText(MainActivity.this, "Please enter both color and date.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                });
-
-                AlertDialog dialog = builder.create();
-                dialog.show();
+                showAddDataDialog();
             }
         });
+
+        // Initialize Firebase Database reference
+        databaseRef = FirebaseDatabase.getInstance().getReference("data");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_sync) {
+            syncDataWithFirebase();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showAddDataDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Add Data");
+
+        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+        View dialogView = inflater.inflate(R.layout.dialog_add_data, null);
+        builder.setView(dialogView);
+
+        EditText etColor = dialogView.findViewById(R.id.etColor);
+        EditText etDate = dialogView.findViewById(R.id.etDate);
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String color = etColor.getText().toString();
+                String date = etDate.getText().toString();
+
+                if (!TextUtils.isEmpty(color) && !TextUtils.isEmpty(date)) {
+                    viewModel.insertData(color, date);
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter both color and date.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void syncDataWithFirebase() {
+        List<DataModel> dataList = viewModel.getDataList().getValue();
+        if (dataList != null && !dataList.isEmpty()) {
+            for (DataModel data : dataList) {
+                // Upload data to Firebase
+                String key = databaseRef.push().getKey();
+                databaseRef.child(key).setValue(data);
+            }
+            Toast.makeText(this, "Data uploaded to Firebase", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "No data to upload", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updatePendingCount(int count) {
+        TextView pendingCountTextView = findViewById(R.id.pendingCountTextView);
+        if (pendingCountTextView != null) {
+            if (count > 0) {
+                pendingCountTextView.setVisibility(View.VISIBLE);
+                pendingCountTextView.setText(String.valueOf(count));
+            } else {
+                pendingCountTextView.setVisibility(View.GONE);
+            }
+        }
+    }
+
 }
